751865faf44eacea4b3e5ebfabc196b0e5068ae7
0x70b1a5 <0x70b1a5@users.noreply.github.com>
2019-04-24 06:47:47 -0400

    SVG magic :sparkles: I see things now! It should theoretically work on some small webpages. But I need to math the extremely large points down in order to display the entire mountain nicely.

--- mtDOM.js
+++ mtDOM.js
@@ -30,15 +30,22 @@ function flattenDeep(arr1) {
 
 function generateTopography(mtn) {
     var X = 0, Y = 0; // absolute X should increment with every new element.
-    var pointArray = mtn.map(function peakToPoint(peak, idx, _, multi, divisor) { // Need to _ the map function's default params for array
+    var pointArray = mtn.map(function peakToPoint(peak, idx, _, multi, divisor) { // Need to _ Array.prototype.map's param for array
         X += (divisor > 0) ? (idx + 1)/divisor : 1; // For elements in nested arrays, their X hops should be 1/N as wide as elements in the root array
         multi = multi || 0;
         Y = Math.max(Y, (peak && peak.elevation || 0));
+        debugger;
         return Array.isArray(peak) ? // peak :: Array
             peak.map(function (p, i, a) { return peakToPoint(p, i, a, multi + 1, peak.length); }) 
-            : Math.round(X * 10) + "," + peak.elevation; // /(multi ? Math.pow(Math.e, multi) : 1) Peaks get shorter as they rise. 
+            : [Math.round(X * 10), peak.elevation]// /(multi ? Math.pow(Math.E, multi) : 1) // Peaks get shorter as they rise. 
     }); 
     var flatBoi = flattenDeep(pointArray);
+    // even-indexed elements are X, odd-indexed elements are Y.
+    // Normalize Y heights to produce a palatable visual.
+    for (var yi = 1; yi < flatBoi.length; yi += 2) {
+        // todo!
+    }
+
     return {
         width: Math.round(X * 10),
         height: Y,
@@ -46,23 +53,26 @@ function generateTopography(mtn) {
     }
 }
 
+function makeSvgEl(tag, attrs) {
+    var el = document.createElementNS('http://www.w3.org/2000/svg', tag);
+    for (var k in attrs)
+        el.setAttribute(k, attrs[k]);
+    return el;
+}
+
 function drawMountain (mtn) {
-    var $svg = document.createElement('svg');
-    $svg.setAttribute('style', "height: 100vh; width: 100vw; pointer-events: none; position: absolute; top: 0; left: 0; z-index: 1337;");
-    $svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
-    $svg.setAttribute('version', '1.1');
-    var $polygon = document.createElement('polygon');
-    var mapStyles = "stroke: #168fdc; stroke-width: 5; fill: #95CFF4; opacity: 0.75;";
-    $polygon.setAttribute('style', mapStyles);
-    
     var topography = generateTopography(mtn);
-
-    $svg.setAttribute('viewBox', '0 0 ' + topography.width + ' ' + topography.height);
-    $polygon.setAttribute('points', topography.points);
-
-    $svg.appendChild($polygon);
     
-    return setTimeout(function() { document.body.appendChild($svg); }, 0);
+    var $polygon = makeSvgEl('polygon', { points: topography.points, stroke: '#168fdc', 'stroke-width': '2', fill: '#95CFF4' });
+    var $svg = makeSvgEl('svg', {
+        height: window.innerHeight,
+        width: window.innerWidth - 32,
+        style: 'pointer-events: none; position: absolute; top: 0; left: 0; z-index: 1337;',
+        version: 1.1,
+        viewBox: '0 0 ' + topography.width + ' ' + topography.height
+    });
+
+    return document.body.appendChild($svg).appendChild($polygon);
 }
 
 var mtn = [];
