caba2189fe9f0d1477d7d08fb298e9453feedd82
0x70b1a5 <0x70b1a5@users.noreply.github.com>
2019-04-23 22:19:23 -0400

    Finalize algo :volcano: now to figure out SVG...

--- mtDOM.js
+++ mtDOM.js
@@ -1,8 +1,6 @@
 // MtDOM.js
-// Written by Tobias Merkle 0x70b1a5 in a fit of insanity 2019
-
-Array.prototype.base = null; // Pointer to the array that this array sits in, if any.
-Array.prototype.elevation = 0; // "height" of array (used in maps later).
+// Written by Tobias Merkle in a fit of insanity. He takes no responsibility for what lies beneath.
+// April 2019
 
 function makeMountain(el, mtn) {
     var children = Array.from(el && el.childNodes || el || '');
@@ -12,11 +10,9 @@ function makeMountain(el, mtn) {
     return el;
 }
 
-function reckonMountainHeight(mtn) {
-
-    //TODO... This is all trash
+function reckonMountainHeight(mtn, addRawLength) {
+    mtn.elevation = (mtn.base ? mtn.base.elevation || 0 : 0) + (mtn.elevation || mtn.length || 0);
     if (Array.isArray(mtn)) {
-        mtn.elevation = (mtn.base ? mtn.base.elevation || 0 : 0) + (mtn.length || 0);
         var nextPeak = [];
         nextPeak.base = mtn;
         nextPeak.elevation = mtn.elevation;
@@ -32,78 +28,45 @@ function flattenDeep(arr1) {
     return arr1.reduce((acc, val) => Array.isArray(val) ? acc.concat(flattenDeep(val)) : acc.concat(val), []);
  }
 
-function findRangeWidth(range) { // :: [Int | Array] | Int
-    return range.length && !range.every(function(peak) { return peak == +peak }) ? 
-        flattenDeep(range).length // [Int | Array]
-        : range.length ? 
-            range.length // [Int]
-            : +range || 0; // Int
-};
-
-function findRangeHeight(range) {
-    var max = 0;
-    if (Array.isArray(range)) {
-        range.forEach(function(peak) {
-            var nextPeakHeight = 0;
-
-            if (Array.isArray(peak)) nextPeakHeight = findRangeHeight(peak);
-            else nextPeakHeight = peak.elevation;
-
-            +peak.elevation && (max = Math.max(nextPeakHeight, max));
-        });
-    } else {
-        +range.elevation && (max = Math.max(range, max));
-    }
-    return max;
-}
-
-function generateTopography(mtn, hSize, vSize) {
-    var peaksAsHeights = mtn.map(function getPeakPixelHeight(peak, idx, array, multi) {
-        debugger;
-        return peak == +peak ? peak
-            : peak && peak.elevation > -1 ? 
-                peak.elevation*vSize/(multi ? Math.pow(Math.e, multi) : 1) // Peaks get shorter as they rise. 
-                : peak.map(function (p) { return getPeakPixelHeight(p, null, null, (multi || 0) + 1); });
+function generateTopography(mtn) {
+    var X = 0, Y = 0; // absolute X should increment with every new element.
+    var pointArray = mtn.map(function peakToPoint(peak, idx, _, multi, divisor) { // Need to _ the map function's default params for array
+        X += (divisor > 0) ? (idx + 1)/divisor : 1; // For elements in nested arrays, their X hops should be 1/N as wide as elements in the root array
+        multi = multi || 0;
+        Y = Math.max(Y, (peak && peak.elevation || 0));
+        return Array.isArray(peak) ? // peak :: Array
+            peak.map(function (p, i, a) { return peakToPoint(p, i, a, multi + 1, peak.length); }) 
+            : Math.round(X * 10) + "," + peak.elevation; // /(multi ? Math.pow(Math.e, multi) : 1) Peaks get shorter as they rise. 
     }); 
-    debugger;
-    var peaksAsHeightWidthPairs = peaksAsHeights.map(function getPeakPixelWidth(heightOrPeak, idx, array, parentElevation, divisor) {
-        return heightOrPeak == +heightOrPeak ?
-            ((parentElevation || 0) + (hSize/(divisor || 1))).toFixed(2) + "," + heightOrPeak
-            : heightOrPeak.map(function (p) { return getPeakPixelWidth(p, null, null, parentElevation || 0, hSize/heightOrPeak.length); })
-                .reduce(function(a, b) { return a + b + " "; }, "");
-    });
-    debugger;
-    return peaksAsHeightWidthPairs.join(' ');
+    var flatBoi = flattenDeep(pointArray);
+    return {
+        width: Math.round(X * 10),
+        height: Y,
+        points: flatBoi.join(' ')
+    }
 }
 
 function drawMountain (mtn) {
     var $svg = document.createElement('svg');
-    $svg.setAttribute('style', "height: 100vh; width: 100vw; pointer-events: none;");
+    $svg.setAttribute('style', "height: 100vh; width: 100vw; pointer-events: none; position: absolute; top: 0; left: 0; z-index: 1337;");
     $svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
     $svg.setAttribute('version', '1.1');
     var $polygon = document.createElement('polygon');
     var mapStyles = "stroke: #168fdc; stroke-width: 5; fill: #95CFF4; opacity: 0.75;";
     $polygon.setAttribute('style', mapStyles);
-
-    var mountainWidth = findRangeWidth(mtn);
-    var mountainHeight = findRangeHeight(mtn);
-    var horizontalUnit = window.innerWidth/mountainWidth;
-    var verticalUnit = window.innerHeight/mountainHeight;
-
-    debugger;
-    var points = generateTopography(mtn, horizontalUnit, verticalUnit);
-    debugger;
-    $polygon.setAttribute('points', points);
-
-    $svg.appendChild(document.createElement('g')).appendChild($polygon);
     
-    return document.body.appendChild($svg);
+    var topography = generateTopography(mtn);
+
+    $svg.setAttribute('viewBox', '0 0 ' + topography.width + ' ' + topography.height);
+    $polygon.setAttribute('points', topography.points);
+
+    $svg.appendChild($polygon);
+    
+    return setTimeout(function() { document.body.appendChild($svg); }, 0);
 }
 
 var mtn = [];
 var rawMtn = makeMountain(document.body, mtn);
 
-debugger;
 mtn = reckonMountainHeight(rawMtn);
-debugger;
 drawMountain(mtn);
