82263cd08ae3b1227178c6e3383bef26b762f31f
0x70b1a5 <0x70b1a5@users.noreply.github.com>
2019-04-25 14:09:49 -0400

    IT WORKS! Disable preserveAspectRatio on the SVG to allow FULL HEIGHT SCALING...

--- mtDOM.js
+++ mtDOM.js
@@ -2,6 +2,9 @@
 // Written by Tobias Merkle in a fit of insanity. He takes no responsibility for what lies beneath.
 // April 2019
 
+var resizeTaskId, $svg; 
+var delay = 100;
+
 function makeMountain(el, mtn) {
     var children = Array.from(el && el.childNodes || el || '');
     el.base = mtn;
@@ -36,26 +39,32 @@ function generateTopography(mtn) {
     // Assuming the current point graph has a large number of remote outliers,
     // the goal of this block should be to proportionally reduce the most freakishly tall peaks
     // such that they are a maximum of around 3-4 standard deviations higher than the rest.
-    var numericalHeights = flattenDeep(mtn).map(function(flattenedPeak) { return flattenedPeak.elevation || (flattenedPeak.base && flattenedPeak.base.elevation) || 0 });
+    var numericalHeights = flattenDeep(mtn).map(function(flattenedPeak) { return (flattenedPeak.elevation || (flattenedPeak.base && flattenedPeak.base.elevation) || 0) });
 
     var avg = numericalHeights.reduce(function(a, b) { 
-        Y = Math.max(Y, b); 
         return a + b; 
     }, 0) / numericalHeights.length;
     var stdDev = Math.sqrt(numericalHeights.reduce(function (a, b) { return a + Math.pow(b - avg, 2) }, 0) / numericalHeights.length);
+    // var sum = Y;
+    // var logSumExp = numericalHeights.map(function (p) { 
+    //     sum += Math.pow(Math.E, p - Y); 
+    //     return Math.log(sum);
+    // });
 
     var pointArray = mtn.map(function peakToPoint(peak, idx, _, multi, divisor) { // Need to _ Array.prototype.map's param for array
-        var elevation = (peak.elevation || (peak.base && peak.base.elevation) || 0) + mtn.length;
-        // if (elevation > 0) elevation /= 1/(Math.pow(Math.E, 1/elevation)); 
-        // if (elevation > (avg + 6*stdDev)) // This magic number is arbitrary, for higher heights increase it.
-        //     elevation /= Math.pow(Math.E, elevation/stdDev);
-
+        var elevation = (peak.elevation || (peak.base && peak.base.elevation) || 0);
+        // Sigmoid! See https://en.wikipedia.org/wiki/Generalised_logistic_function for why we are multiplying by the various values - we really want to "buff up" small values.
+        elevation /= 1/(1 + Math.pow(Math.E, 0.1 * (-elevation)));
+        if (elevation > (avg + 3*stdDev)) // This magic number is arbitrary, for even higher heights increase it.
+            elevation = avg + 4*stdDev;
+        
+        Y = Math.max(Y, elevation); // obviously, we want to calculate maximum Y here rather than before all these reduction calculations.
         X += (divisor > 0) ? (idx + 1)/divisor : 1; // For elements in nested arrays, their X hops should be 1/N as wide as elements in the root array.
         multi = multi || 0;
 
         return Array.isArray(peak) ? // peak :: Array
             peak.map(function (p, i, a) { return peakToPoint(p, i, a, multi + 1, peak.length); }) 
-            : Math.round(X * 10) + ',-' + elevation // negative Y to draw peaks 'northward'.
+            : Math.round(X * 10) + ',-' + Math.round(elevation) // negative Y to draw peaks 'northward'.
     }); 
 
     var finalX = Math.round(X * 10) + 10;
@@ -63,7 +72,7 @@ function generateTopography(mtn) {
 
     return {
         width: finalX,
-        height: Y, // not Y, because we normalized.
+        height: Y, 
         points: stringifiedXYPoints
     }
 }
@@ -80,20 +89,48 @@ function drawMountain (mtn) {
     
     var $polygon = makeSvgEl('polygon', { 
         points: topography.points, 
-        stroke: '#168fdc', 
-        'stroke-width': '2', 
+        style: 'animation: loom 5000ms infinite ease-in-out;',
         fill: '#95CFF4',
-    });
-    var $svg = makeSvgEl('svg', {
+    })
+    // var $animation = makeSvgEl('animate', {
+    //     attributeType: 'CSS',
+    //     attributeName: 'fill',
+    //     values: 'linear-gradient(135deg, rgba(255,0,255,.4) 0%, rgba(255,255,255,0) 50%, rgba(255,255,255,0) 100%);\
+    //     linear-gradient(135deg, rgba(255,255,255,0) 0%, rgba(255,0,255,.4) 50%, rgba(255,255,255,0) 100%);\
+    //     linear-gradient(135deg, rgba(255,255,255,0) 0%, rgba(255,255,255,0) 50%, rgba(255,0,255,.4) 100%);',
+    //     dur: '5s',
+    //     repeatCount: 'indefinite'
+    // }).appendChild(makeSvgEl('linearGradient', {
+        
+    // })).appendChild(makeSvgEl('stop', {
+    //     offset: '5%',
+    //     'stop-color': '#F0F'
+    // }));
+    // $polygon.append($animation);
+    $svg = makeSvgEl('svg', {
         height: window.innerHeight,
-        width: window.innerWidth,
+        width: window.innerWidth - 16,
         version: 1.1,
-        style: 'pointer-events: none; position: fixed; bottom: 0; left: 0; z-index: 1337;',
-        viewBox: '0 ' + (-topography.height) + ' ' + window.innerWidth + ' ' + window.innerHeight
+        preserveAspectRatio: 'none',
+        style: 'pointer-events: none; position: fixed; bottom: 0; left: 0; z-index: 1337; shape-rendering: optimizeSpeed; opacity: 0.25; ',
+        viewBox: '0 -' + topography.height + ' ' + topography.width + ' ' + topography.height
     });
 
+    window.addEventListener('resize', function(e) {
+        if (resizeTaskId)  clearTimeout(resizeTaskId);
+        resizeTaskId = setTimeout(function() {
+            onResize(e);
+        }, delay);
+    });
     // todo ... SVG viewbox should adjust downward instead of scaling polygon upwards
     return document.body.appendChild($svg).appendChild($polygon);
+}
+
+function onResize(e) {
+    console.log('onresize');
+    if (!$svg) return;
+    $svg.setAttribute('height', window.innerHeight);
+    $svg.setAttribute('width', window.innerWidth);
 }
 
 var mtn = [];
